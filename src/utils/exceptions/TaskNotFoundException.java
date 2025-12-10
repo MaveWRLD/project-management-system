6706ae3a72e3d5b582f1d8304917f4053b51a408
Jacob Quaye <kofimave@gmail.com>
2025-12-10 09:26:13 +0000

    feature: updated class diagram

--- src/utils/exceptions/TaskNotFoundException.java
+++ src/utils/exceptions/TaskNotFoundException.java
@@ -1,7 +1,7 @@
 package utils.exceptions;
 
 public class TaskNotFoundException extends Exception{
-    public TaskNotFoundException(String message) {
-        super(message);
+    public TaskNotFoundException(String projectId, String taskId) {
+        super("Project with id " + projectId + " has no associated task with id " + taskId);
     }
 }
